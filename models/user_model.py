cefd059ad05976e286f0addbafbcc3a37a6f38af
Denis Cercasin <deniscercasin@gmail.com>
2025-06-22 14:33:40 +0200

    refactor: restructures the user model for sqlalchemy use

--- models/user_model.py
+++ models/user_model.py
@@ -1,32 +1,27 @@
 from flask_login import UserMixin
-from db import get_db_con
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_base import alchemy
 
-class User(UserMixin):
-    def __init__(self, id, name, email, reminder_day = None, reminder_enabled = False):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.reminder_day = reminder_day
-        self.reminder_enabled = bool(reminder_enabled)
+class User(alchemy.Model, UserMixin):
+    __tablename__ = "user"
 
-def get_user_by_id(user_id):
-    conn = get_db_con()
-    user_row = conn.execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
-    if user_row:
-        return User(user_row["id"], user_row["name"], user_row["email"],user_row["reminder_day"],user_row["reminder_enabled"])
-    return None
+    id = alchemy.Column(alchemy.Integer, primary_key=True)
+    name = alchemy.Column(alchemy.String, nullable=False)
+    email = alchemy.Column(alchemy.String, unique=True, nullable=False)
+    password = alchemy.Column(alchemy.String, nullable=False)
+    reminder_day = alchemy.Column(alchemy.Integer, nullable=True)
+    reminder_enabled = alchemy.Column(alchemy.Boolean, default=False)
 
-def get_user_by_email(email):
-    conn = get_db_con()
-    user_row = conn.execute("SELECT * FROM user WHERE email = ?", (email,)).fetchone()
-    if user_row:
-        return User(user_row["id"], user_row["name"], user_row["email"])
-    return None
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        print("was set")
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+@staticmethod
 def update_password_for_email(email, new_password):
-    conn = get_db_con()
-    hashed_pw = generate_password_hash(new_password)
-    conn.execute("UPDATE user SET password = ? WHERE email = ?", (hashed_pw, email))
-    conn.commit()
-    conn.close()
+    user = User.query.filter_by(email=email).first()
+    if user:
+        user.set_password(new_password)
+        alchemy.session.commit()
